dbe574e422eec240c8b1a61b5818e72d37915537
vil141 <j.villanueva5@utexas.edu>
2022-01-07 21:02:53 -0600

    Python practice to be used towards analysis

---
+++ Python_Practice.py
@@ -0,0 +1,20 @@
+print("Hello World!")
+counties = ["Arapahoe","Denver", "Jefferson"]
+    print("counties = ["Arapahoe","Denver", "Jefferson"] Initialized")
+    print("displaying index locations")
+    counties[0]
+    print(counties[0])
+    counties[2]
+    print(counties[2])
+    print(counties[-1])
+    len(counties)
+    print("len(counties) gives length of list")
+    counties[0:2] 
+    print("to find the first and second items from counties")
+counties.append("El Paso")
+print("added El Paso to the List")
+counties.insert(2, "El Paso")
+print("specifies where the new item is added in index")
+print("counties.remove('ElPaso') would remove")
+print("counties.pop(3) or pop(-1) would remove as well")
+counties[2]="El Paso"
